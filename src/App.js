91c36334efc5c6b1f21d16898c31cee9d10e55ba
emilionc <89553626+emilionc@users.noreply.github.com>
2021-09-17 13:20:23 -0300

    tarea asignada

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>hola mundo </p>
+        <p>saludo comision 19740</p>
         <a
           className="App-link"
           href="https://reactjs.org"
