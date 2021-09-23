edca77ce1b36d90c5ff7ea2c9da40cba0718ab00
emilionc <89553626+emilionc@users.noreply.github.com>
2021-09-23 16:20:14 -0300

    navbar y icon

---
+++ src/Components/CardWidget/CardWidget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Icon } from 'semantic-ui-react'
+import  './CardWidget.css';
+
+
+const CardWidget = () => 
+
+<div className="CardWidget">
+<h1><Icon disabled name='cart' /></h1>
+</div>
+export default CardWidget
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className="CardWidget">
+        </div>
+    )
+}
+
+export default NavBar
